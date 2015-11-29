6e56183235c660cdcdeda24034598a9fdc1fd3a9
VovaFender <vladimir.fiction@gmail.com>
2015-11-30 02:05:52 +0300

    init commit for lab01

---
+++ Math.hs
@@ -0,0 +1,40 @@
+module Math
+(
+	euclideanDistance,
+	hammingDistance,	
+	multNumOnVector,	
+	subtractMatrices,
+	vectorsSum,
+	matrixNorm,
+	transposeMatrix,
+	rollFraction
+) where
+
+euclideanDistance :: (Floating a) => [a] -> [a] -> a
+euclideanDistance xs ys = sqrt . sum . map (^2) $ zipWith (-) xs ys
+
+hammingDistance :: (Floating a) => [a] -> [a] -> a
+hammingDistance xs ys = sum . map (abs) $ zipWith (-) xs ys
+
+multNumOnVector :: (Num a) => a -> [a] -> [a]
+multNumOnVector n xs = map (*n) xs
+
+subtractMatrices :: (Num a) => [[a]] -> [[a]] -> [[a]]
+subtractMatrices = zipWith (zipWith (-))
+
+vectorsSum :: Num a => [[a]] -> [a]
+vectorsSum = foldl1 (zipWith (+))
+
+matrixNorm :: (Num a, Ord a) => [[a]] -> a
+matrixNorm = maximum . map (maximum) . absMatrix
+
+absMatrix :: (Num a) => [[a]] -> [[a]]
+absMatrix = map (map (abs))
+
+transposeMatrix :: (Num a) => [[a]] -> [[a]]
+transposeMatrix [[]] = []
+transposeMatrix ([]:_) = []
+transposeMatrix xs = map (head) xs : transposeMatrix (map (tail) xs)
+
+rollFraction :: (Fractional a) => a -> a
+rollFraction = (/) 1
---
+++ lab01.hs
@@ -0,0 +1,95 @@
+import Prelude hiding (catch)
+import Fcm
+import System.Random
+import System.Environment
+import System.IO
+import System.IO.Error
+import qualified Data.Map as Map
+import Data.Maybe as Maybe
+import Data.List.Split as Split
+import Control.Exception as Exception
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+stringsToMap :: (Ord a) => [[a]] -> Map.Map [a] [a]
+stringsToMap [] = Map.empty
+stringsToMap xs = Map.fromList $ stringsToMap' xs
+	where
+		stringsToMap' ([]) = []
+		stringsToMap' (x:[]) = (x, []) : []
+		stringsToMap' (x:xs) = (x, head xs) : stringsToMap' (tail xs)
+
+printResult :: (Show a) => Maybe String -> a -> IO ()
+printResult fileName result
+	| Maybe.isNothing fileName = print result
+ 	| otherwise = writeFile (fromJust fileName) (show result)
+
+cutOff :: Bool -> ([a] -> [a]) -> [a] -> [a]
+cutOff p f xs
+	| p == True = f xs
+	| otherwise = xs
+
+splitWithIgnore :: (Bool, Bool) -> String -> String -> [String]
+splitWithIgnore ifs s = 
+	ignoreLastColumn (snd ifs)
+	. ignoreFirstColumn (fst ifs)
+	. Split.splitOn s
+		where 
+			ignoreFirstColumn ifl = cutOff ifl tail
+			ignoreLastColumn ifl = cutOff ifl init
+
+ioHandler :: IOException -> IO String
+ioHandler e
+	| isDoesNotExistError e = error "Vectors input file does not exist"
+	| otherwise = ioError e
+
+parseCSV :: String -> (Bool, Bool, Bool) -> FilePath -> IO [[String]]
+parseCSV s ifs fp = do
+	fc <- readFile fp `catch` ioHandler
+	return $ 
+		map (splitWithIgnore (second ifs, third ifs) s)
+		. ignoreFirstRow (first ifs)
+		. lines $ fc
+			where ignoreFirstRow ifl = cutOff ifl tail				
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of
+				[(x, "")] -> Just x
+				_ -> Nothing
+
+parseArgs :: IO (Int, Float, String, Map.Map String String)
+parseArgs = do
+	args <- getArgs
+
+	let numberOfClusters = read (args !! 0) :: Int
+	let precision = read (args !! 1) :: Float
+	let fileName = args !! 2
+
+	return (numberOfClusters, precision, fileName, stringsToMap $ drop 3 args)
+
+main = do
+
+	(numberOfClusters, precision, fileName, extraArgsMap) <- parseArgs
+
+	let csvSeparator = Map.findWithDefault "," "-fsp" extraArgsMap
+	
+	let startMethod = read (Map.findWithDefault "Weights" "-sm" extraArgsMap) :: StartMethod
+	let distanceMethod = read (Map.findWithDefault "Euclidean" "-d" extraArgsMap) :: VectorDistance
+	let csvIgnoreFirstRow = read (Map.findWithDefault "False" "-ifr" extraArgsMap) :: Bool
+	let csvIgnoreLastColumn = read (Map.findWithDefault "False" "-ilc" extraArgsMap) :: Bool
+	let csvIgnoreFirstColumn = read (Map.findWithDefault "False" "-ifc" extraArgsMap) :: Bool
+
+	csvParsed <- parseCSV csvSeparator
+		(csvIgnoreFirstRow, csvIgnoreFirstColumn, csvIgnoreLastColumn) fileName
+
+	let vectors = map (map (\x -> read x :: Float)) csvParsed
+	let classifyResult = classify startMethod distanceMethod numberOfClusters precision vectors
+
+	printResult (Map.lookup "-out" extraArgsMap) classifyResult
